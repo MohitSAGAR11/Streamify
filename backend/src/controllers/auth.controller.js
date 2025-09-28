135140ad8edca682ab6dd18b5e00cc6dfdd720df
Mohit Sagar <mohitsagar378@gmail.com>
2025-09-28 16:59:47 +0530

    db connected , stream api key saved and user model created

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req , res) {
+    res.send("SignUp")
+}
+
+export async function login(req , res) {
+    res.send("Login")
+}
+
+export async function logout(req , res) {
+    res.send("logout")
+}
