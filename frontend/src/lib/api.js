71048dc8852ecac87be90f7032beaac230d0f1df
Mohit Sagar <mohitsagar378@gmail.com>
2025-10-23 23:17:58 +0530

    error solved in recommendedUser and HomePage made

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -10,7 +10,7 @@ export const getAuthUser = async () => {
     const response = await axiosInstance.get("/auth/me");
     return response.data;
   } catch (error) {
-    console.log("Error in getAuthUser" , error)
+    console.log("Error in getAuthUser", error);
     return null;
   }
 };
@@ -26,7 +26,43 @@ export const login = async (loginData) => {
 };
 
 export const logout = async () => {
-    const response = await axiosInstance.put("/auth/logout");
-    return response.data;
+  const response = await axiosInstance.put("/auth/logout");
+  return response.data;
+};
+
+export async function getUserFriends() {
+  const response = await axiosInstance.get("/users/friends");
+  return response.data;
 }
 
+export async function getRecommendedUsers() {
+  const response = await axiosInstance.get("/users");
+  return response.data.recommendedUsers;
+}
+
+export async function getOutgoingFriendReqs() {
+  const response = await axiosInstance.get("/users/outgoing-friend-requests");
+  return response.data;
+}
+
+export async function sendFriendRequest(userId) {
+  const response = await axiosInstance.post(`/users/friend-request/${userId}`);
+  return response.data;
+}
+
+export async function getFriendRequests() {
+  const response = await axiosInstance.get("/users/friend-requests");
+  return response.data;
+}
+
+export async function acceptFriendRequest(requestId) {
+  const response = await axiosInstance.put(
+    `/users/friend-request/${requestId}/accept`
+  );
+  return response.data;
+}
+
+export async function getStreamToken() {
+  const response = await axiosInstance.get("/chat/token");
+  return response.data;
+}
